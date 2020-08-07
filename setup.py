b75f051e04e670aa1206d48a7ad21a3a90a450bc
Marco Caselli <casellimarco7@gmail.com>
2020-08-07 17:21:29 +0100

    Make package wheel fail

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_namespace_packages
 from setuptools_rust import Binding, RustExtension
 
 
 setup(
     name='dummy',
     version="0",
-    rust_extensions=[RustExtension("hello", path="Cargo.toml", binding=Binding.PyO3, debug=False)],
-    install_requires=['numpy',
-        "google-cloud-storage",
-        "apache_beam",
-        "jsonschema",
-        "inflection",
-                      'PyYAML',
-                      'hdbscan'],
+    packages=find_namespace_packages(include=['dummy.*']),
+    namespace_packages=['dummy'],
+    rust_extensions=[RustExtension("dummy.hello", path="Cargo.toml", binding=Binding.PyO3, debug=False)],
 )
 
