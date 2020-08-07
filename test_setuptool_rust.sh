0f1e38832ee42b1558ee6902a843af73ec7b96ef
Marco Caselli <casellimarco7@gmail.com>
2020-08-07 17:21:39 +0100

    Update bash script

--- test_setuptool_rust.sh
+++ test_setuptool_rust.sh
@@ -4,6 +4,7 @@ set -xeuf -o pipefail
 rm -rf venv
 
 python3.6 -m venv venv
+source venv/bin/activate
 pip install setuptools==40.3.0 setuptools_rust==0.11.0 wheel pip --upgrade
 
 python setup.py bdist_wheel
